f91d29485f34458820eac122942bc6f1c0faa75f
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2023-09-11 20:28:11 -0500

    Update BalancedBracketsTest.java

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void testBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launchcode]"), "Verify [Launchcode] returns True");
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"), "Verify Launch[Code] returns True");
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launchcode"), "Verify []Launchcode returns True");
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""), "Verify '' returns True");
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"), "Verify [] returns True");
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"), "Verify [[[]]] returns True");
+    }
+
+    @Test
+    public void testImbalanced() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"), "Verify [Launchcode returns False");
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["), "Verify Launch]code[ returns False");
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["), "Verify [ returns False");
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["), "Verify ][ returns False");
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"),"Verify ] returns False");
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]]"), "Verify [[[]] returns False");
     }
 }
